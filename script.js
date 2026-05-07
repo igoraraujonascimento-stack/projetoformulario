e44783b5c29e921e95ac114f959649117f312b06
Igor Araújo do Nascimento <igor.araujo.nascimento@escola.pr.gov.br>
2026-05-07 08:28:55 -0300

    criando html

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function salvarCarro(event) {
     let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
 
     let carro = {
+        id: Date.now(),
         titulo,
         preco,
         marca,
@@ -18,11 +19,50 @@ function salvarCarro(event) {
         cambio
     };
 
-    let carros = JSON.parse(localStorage.getitem("carros")) || [];
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
     carros.push(carro);
     localStorage.setItem("carros", JSON.stringify(carros));
 
     adicionarNaTela(carro);
 
     document.querySelector("form").reset();
+}
+
+function adicionarNaTela(carro) {
+    let lista = document.getElementById('listaCarros');
+    let card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+    <img src = "https://picsum.photos/250/150?random = ${Math.random()}">
+    <h3>${carro.titulo}</h3>
+    <p><strong>Preço:</strong> R$ ${carro.preco}</p>
+    <p><strong>Marca:</strong> ${carro.marca}</p>
+    <p><strong>Modelo:</strong> ${carro.modelo}</p>
+    <p><strong>Câmbio:</strong> ${carro.cambio}</p>
+
+    <button onclick = "excluirCarro(${carro.id}">Excluir</button>
+    `
+
+    lista.appendChild(card);
+}
+
+window.onload = function () {
+    let carros = JSON.parse(localStorage.getItem('carros')) || [];
+
+    carros.forEach(carro => {
+        adicionarNaTela(carro);
+    });
+}
+
+function excluirCarro(id) {
+    let carros = JSON.parse(localStorage.getItem('carros')) || [];
+
+    carros = carros.filter(carro => Number(carro.id) !== Number(id));
+
+    localStorage.setItem('carros', JSON.stringify(carros));
+
+    document.getElementById('listaCarros').innerHTML = "";
+
+    carros.forEach(carro => adicionarNaTela(carro));
 }
