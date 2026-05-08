5f972acd255dd9ebd8d17ec1beb4c5dcca8c32c2
Igor Araújo do Nascimento <igor.araujo.nascimento@escola.pr.gov.br>
2026-05-08 10:21:47 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -7,29 +7,71 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//conexão com o banco de dados
+//conexão com banco
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '', // sua senha
     database: 'loja_carro'
 });
 
-//teste
-app.get('/test', (req, res) => {
-    res.send('API Funcionando');
+// 🔹 TESTE
+app.get('/', (req, res) => {
+    res.send('API funcionando');
 });
 
-//salvar carros
+
+// SALVAR CARRO
 app.post('/carros', (req, res) => {
-    const { titulo, preco, marca, modelo, cambio } = req.body;
-    const sql = 'INSERT INTO carros (titulo, preco, marca, modelo, cambio) VALUES (?, ?, ?, ?, ?)';
-    db.query(sql, [titulo, preco, marca, modelo, cambio], (err, result) => {
+
+    const { titulo, preco, descricao, marca, modelo, kilometragem, data_compra, cambio } = req.body;
+
+    const sql = `
+        INSERT INTO carros 
+        (titulo, preco, descricao, marca, modelo, kilometragem, data_compra, cambio)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    db.query(sql, [titulo, preco, descricao, marca, modelo, kilometragem, data_compra, cambio], (err, result) => {
         if (err) {
             console.log(err);
-            res.status(500).send('Erro ao salvar o carro');
+            res.status(500).send('Erro');
         } else {
             res.send(result);
         }
     });
+});
+
+
+//LISTAR CARROS
+app.get('/carros', (req, res) => {
+
+    db.query('SELECT * FROM carros', (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(result);
+        }
+    });
+
+});
+
+
+// EXCLUIR
+app.delete('/carros/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    db.query('DELETE FROM carros WHERE id = ?', [id], (err) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send('Deletado');
+        }
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
 });
